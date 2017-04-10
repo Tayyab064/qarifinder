937b25544b7d693af1a7caa6f359e3f9eee95cb6
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2017-04-10 19:18:41 +0500

    chuss

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
 	has_secure_token :token
 	has_secure_token :password_reset_token
 
-	reverse_geocoded_by :latitude, :longitude
+	reverse_geocoded_by :latitude, :longitude do |obj,results|
+		if geo = results.first
+		  obj.place = geo.address
+		end
+	end
 	after_validation :reverse_geocode
 
 	has_many :events , as: :eventby , dependent: :destroy

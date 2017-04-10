9ea5aae53f30d5c461ad2e2357f51eb7e6a744f9
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2017-04-10 19:39:50 +0500

    push

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,10 @@
 class Event < ApplicationRecord
-	geocoded_by :place
-	after_validation :geocode
+	reverse_geocoded_by :latitude, :longitude do |obj,results|
+		if geo = results.first
+		  obj.place = geo.address
+		end
+	end
+	after_validation :reverse_geocode
 
 	belongs_to :eventby , :polymorphic => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,7 @@ class User < ApplicationRecord
 	has_secure_token :token
 	has_secure_token :password_reset_token
 
-	reverse_geocoded_by :latitude, :longitude do |obj,results|
-		if geo = results.first
-		  obj.place = geo.address
-		end
-	end
+	reverse_geocoded_by :latitude, :longitude
 	after_validation :reverse_geocode
 
 	has_many :events , as: :eventby , dependent: :destroy
